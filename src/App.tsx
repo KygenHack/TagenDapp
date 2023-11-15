adf1faa64c3775c2292bcff7679aa2a7d2ffa1cb
KygenHack <148578932+KygenHack@users.noreply.github.com>
2023-11-15 14:58:27 +0100

    update

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import "./styles/Home.css";
-import Navbar from "./components/Navbar";
-
-export default function Home() {
-  return (
-     <Navbar/>
-  );
-}
